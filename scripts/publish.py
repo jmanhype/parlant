751202c1a7c55ebc4a2e66b5144f8638dab18d8c
Yam Marcovitz <yam@emcie.co>
2024-08-28 17:34:18 +0300

    Build server Docker image when publishing

--- scripts/publish.py
+++ scripts/publish.py
@@ -1,6 +1,31 @@
 #!/usr/bin/python3
 import sys
-from utils import die, for_each_package, Package
+import subprocess
+import toml  # type: ignore
+from utils import die, for_each_package, Package, get_packages
+
+
+def get_server_version() -> str:
+    server_package = next(p for p in get_packages() if p.name == "server")
+    project_file = server_package.path / "pyproject.toml"
+    pyproject = toml.load(project_file)
+    version = str(pyproject["tool"]["poetry"]["version"])
+    return version
+
+
+def publish_docker() -> None:
+    version = get_server_version()
+
+    build_process = subprocess.Popen(
+        args=["docker", "build", "-t", version, ".", "-f", "Dockerfile.server"],
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+    )
+
+    status = build_process.wait()
+
+    if status != 0:
+        die("error: docker build failed")
 
 
 def publish_package(package: Package) -> None:
@@ -22,4 +47,5 @@ def publish_package(package: Package) -> None:
 
 
 if __name__ == "__main__":
+    publish_docker()
     for_each_package(publish_package)
