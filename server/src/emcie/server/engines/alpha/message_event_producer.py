6cad6a95af66601ac018188c889f288c79dfe08a
Yam Marcovitz <yam@emcie.co>
2024-09-11 14:31:13 +0000

    Improve message production quality

--- server/src/emcie/server/engines/alpha/message_event_producer.py
+++ server/src/emcie/server/engines/alpha/message_event_producer.py
@@ -114,7 +114,7 @@ class MessageEventProducer:
         builder.add_section(
             """
 You must generate your reply message to the current (latest) state of the interaction.
-IMPORTANT: Strive to continue the interaction/conversation in the most natural way for a human conversation.
+IMPORTANT: Strive to continue the interaction/conversation in the most natural way for a normal human conversation.
 """
         )
 
@@ -127,12 +127,22 @@ Produce a valid JSON object in the following format: ###
 {{
     “last_message_of_user”: “<the user’s last message in the interaction>”,
     "produced_reply": true,
+    "evaluations_for_each_of_the_provided_guidelines": [
+        {
+            "number": <GUIDELINE_NUMBER>,
+            "instruction": <THE_INSTRUCTION_GIVEN_BY_THE_GUIDELINE>,
+            "evaluation": <EXPLANATION_TO_WHAT_EXTENT_THIS_GUIDELINE_IS_RELEVANT_TO_YOUR_REPLY_NOW>,
+            "adds_value": <EXPLANATION_OF_WHETHER_FOLLOWING_THIS_GUIDELINE_IN_THE_NEXT_MESSAGE_ADDS_NEW_VALUE_TO_THE_INTERACTION_THAT_WAS_NOT_ALREADY_COMMUNICATED>,
+            "data_available": <EXPLANATION_OF_WHETHER_THE_REQUIRED_DATA_IS_AVAILABLE_OR_MISSING_IN_THE_PROVIDED_CONTEXT>
+        },
+        ...
+    ],
     "rationale": "<a few words to justify why you decided to respond in this way>",
     "revisions": [
         {
             "revision_number": <1 TO N>,
             "content": "<your message here>",
-            "followed_all_rules": true
+            "followed_all_guidelines": true
         }
     ]
 }}
@@ -142,18 +152,26 @@ Produce a valid JSON object in the following format: ###
         else:
             builder.add_section(
                 f"""
-Propose revisions to the message content,
-ensuring that your proposals adhere to each and every one of the provided rules based on the most recent state of interaction.
-Consider the priority scores assigned to each rule, acknowledging that in some cases, adherence to a higher-priority rule may necessitate deviation from another.
-Additionally, recognize that if a rule cannot be adhered to due to lack of necessary context or data, this must be clearly justified in your reply.
+Propose incremental revisions to your reply, ensuring that your proposals adhere
+to each and every one of the provided guidelines based on the most recent state of interaction.
+
+Mind the priority scores assigned to each guideline, acknowledging that in some cases,
+adherence to a higher-priority guideline may necessitate deviation from another.
+Use your best judgement in applying prioritization.
+Note too that it is permissible for the final revision to break rules IF AND ONLY IF
+all of the broken rules were broken due to conscious prioritization of guidelines,
+and not due to overlooking or missing a detail in one of the guidelines.
+
+Additionally, recognize that if a guideline cannot be adhered to due to lack of necessary
+context or data, this must be clearly justified in your reply.
 
 Continuously critique each revision to refine the reply.
 Ensure each critique is unique to prevent redundancy in the revision process.
 
 Your final output should be a JSON object documenting the entire message development process.
-This document should detail how each rule was adhered to,
-instances where one rule was prioritized over another,
-situations where rules could not be followed due to lack of context or data,
+This document should detail how each guideline was adhered to,
+instances where one guideline was prioritized over another,
+situations where guidelines could not be followed due to lack of context or data,
 and the rationale for each decision made during the revision process.
 
 DO NOT PRODUCE MORE THAN 5 REVISIONS. IF YOU REACH the 5th REVISION, STOP THERE.
@@ -173,60 +191,106 @@ Example 2: A reply that took critique in a few revisions to get right: ###
 {{
     “last_message_of_user”: “<the user’s last message in the interaction>”,
     "produced_reply": true,
-    "rationale": "<a few words to justify why you decided to respond in this way>",
+    "rationale": "<a few words to justify why you decided to respond to the user at all>",
+    "evaluations_for_each_of_the_provided_guidelines": [
+        {{
+            "number": 1,
+            "instruction": "Do this [...]",
+            "evaluation": "in this situation, I am instructed to do [...]",
+            "adds_value": "I didn't do it yet, so I should do it now"
+            "data_available": "no particular data is needed for this"
+        }},
+        {{
+            "number": 2,
+            "instruction": "Say this [...]",
+            "evaluation": "in this situation, I am instructed to say [...]",
+            "adds_value": "I didn't say it yet, so I should say it now"
+            "data_available": "no particular data is needed for this"
+        }},
+        {{
+            "number": 3,
+            "instruction": "Say that [...]",
+            "evaluation": "in this situation, I am instructed to say [...]",
+            "adds_value": "I didn't say it yet, so I should say it now"
+            "data_available": "no particular data is needed for this"
+        }},
+        {{
+            "number": 4,
+            "instruction": "Do that [...]",
+            "evaluation": "in this situation, I am instructed to do [...]",
+            "adds_value": "I didn't do it yet, so I should do it now"
+            "data_available": "no particular data is needed for this"
+        }}
+    ],
     "revisions": [
         {{
             "revision_number": 1,
             "content": "some proposed message content",
-            "rules_followed": [
+            "guidelines_followed": [
                 "#1; correctly did...",
                 "#3; correctly said..."
             ],
-            "rules_broken": [
-                "#5; didn't do...",
+            "guidelines_broken": [
                 "#2; didn't say..."
+                "#4; didn't do...",
             ],
-            "followed_all_rules": false,
-            "rules_broken_due_to_missing_data": false,
-            "rules_broken_due_to_prioritization": false
+            "followed_all_guidelines": false,
+            "guidelines_broken_due_to_missing_data": false,
+            "guidelines_broken_only_due_to_prioritization": false
         }},
         ...,
         {{
             "revision_number": 2,
             "content": "final verified message content",
-            "rules_followed": [
+            "guidelines_followed": [
                 "#1; correctly did...",
                 "#2; correctly said...",
                 "#3; correctly said...",
                 "#5; correctly did..."
             ],
-            "rules_broken": [],
-            "followed_all_rules": true
+            "guidelines_broken": [],
+            "followed_all_guidelines": true
         }},
     ]
 }}
 
 ###
 
-Example 3: A reply where one rule was prioritized over another: ###
+Example 3: A reply where one guideline was prioritized over another: ###
 {{
     “last_message_of_user”: “<the user’s last message in the interaction>”,
     "produced_reply": true,
-    "rationale": "<a few words to justify why you decided to respond in this way>",
+    "rationale": "<a few words to justify why you decided to respond to the user at all>",
+    "evaluations_for_each_of_the_provided_guidelines": [
+        {{
+            "number": 1,
+            "instruction": "When the user chooses and orders a burger, then provide it"
+            "evaluation": "The user asked for a burger with cheese, so I need to provide it to him.",
+            "adds_value": "I didn't provide the burger yet, so I should do so now."
+            "data_available": "The burger choice is available in the interaction"
+        }},
+        {{
+            "number": 2,
+            "instruction": "When the user chooses specific ingredients on the burger, only provide those ingredients if we have them fresh in stock; otherwise, reject the order."
+            "evaluation": "The user chose cheese on the burger, but all of the cheese we currently have is expired",
+            "adds_value": "I must reject the order, otherwise the user might eat bad cheese"
+            "data_available": "The relevant stock availability is given in the tool calls' data"
+        }}
+    ],
     "revisions": [
         {{
             "revision_number": 1,
             "content": "I'd be happy to prepare your burger as soon as we restock the requested toppings.",
-            "rules_followed": [
-                "#2; upheld food quality and did not prepare the burger without the fresh toppings."
+            "guidelines_followed": [
+                "#2; upheld food quality and did not go on to preparing the burger without fresh toppings."
             ],
-            "rules_broken": [
+            "guidelines_broken": [
                 "#1; did not provide the burger with requested toppings immediately due to the unavailability of fresh ingredients."
             ],
-            "followed_all_rules": false,
-            "rules_broken_due_to_prioritization": true,
-            "prioritization_rationale": "Given the higher priority score of Rule 2, maintaining food quality standards before serving the burger is prioritized over immediate service.",
-            "rules_broken_due_to_missing_data": false
+            "followed_all_guidelines": false,
+            "guidelines_broken_only_due_to_prioritization": true,
+            "prioritization_rationale": "Given the higher priority score of guideline 2, maintaining food quality standards before serving the burger is prioritized over immediate service.",
+            "guidelines_broken_due_to_missing_data": false
         }}
     ]
 }}
@@ -237,20 +301,29 @@ Example 4: Non-Adherence Due to Missing Data: ###
 {{
     “last_message_of_user”: “<the user’s last message in the interaction>”,
     "produced_reply": true,
-    "rationale": "<a few words to justify why you decided to respond in this way>",
+    "rationale": "<a few words to justify why you decided to respond to the user at all>",
+    "evaluations_for_each_of_the_provided_guidelines": [
+        {{
+            "number": 1,
+            "instruction": "When the user asks for a drink, check the menu and offer what's on it"
+            "evaluation": "The user did ask for a drink, so I should check the menu to see what's available.",
+            "adds_value": "The user doesn't know what drinks we have yet, so I should tell him."
+            "data_available": "No, I don't have the menu info in the interaction or tool calls"
+        }}
+    ],
     "revisions": [
         {{
             "revision_number": 1,
             "content": "I'm sorry, I am unable to provide this information at this time.",
-            "rules_followed": [
+            "guidelines_followed": [
             ],
-            "rules_broken": [
-                "#1; Lacking menu data in the context prevented providing the client with drink information."
+            "guidelines_broken": [
+                "#1; Lacking menu data in the context prevented me from providing the client with drink information."
             ],
-            "followed_all_rules": false,
-            "rules_broken_due_to_missing_data": true
+            "followed_all_guidelines": false,
             "missing_data_rationale": "Menu data was missing",
-            "rules_broken_due_to_prioritization": false
+            "guidelines_broken_due_to_missing_data": true
+            "guidelines_broken_only_due_to_prioritization": false
         }}
     ]
 }}
@@ -275,18 +348,32 @@ Example 4: Non-Adherence Due to Missing Data: ###
         if not json_content["produced_reply"]:
             return None
 
-        self.logger.debug(
-            f'Message event producer response: {json.dumps(json_content["revisions"], indent=2)}'
+        revisions = json_content["revisions"]
+
+        self.logger.debug(f"Message event producer response: {json.dumps(json_content, indent=2)}")
+
+        if last_correct_revision := next(
+            (
+                r
+                for r in reversed(revisions)
+                if r.get("guidelines_broken_only_due_to_prioritization")
+            ),
+            None,
+        ):
+            # Sometimes the LLM continues generating revisions even after
+            # it generated a correct one. Those next revisions tend to be
+            # faulty, as they do not handle prioritization well.
+            # This is a workaround.
+            final_revision = last_correct_revision
+        else:
+            final_revision = json_content["revisions"][-1]
+
+        followed_all_guidelines = final_revision.get("followed_all_guidelines", False)
+        guidelines_broken_only_due_to_prioritization = final_revision.get(
+            "guidelines_broken_only_due_to_prioritization", False
         )
 
-        final_revision = json_content["revisions"][-1]
-
-        followed_all_rules = final_revision.get("followed_all_rules", False)
-        rules_broken_due_to_prioritization = final_revision.get(
-            "rules_broken_due_to_prioritization", False
-        )
-
-        if not followed_all_rules and not rules_broken_due_to_prioritization:
+        if not followed_all_guidelines and not guidelines_broken_only_due_to_prioritization:
             self.logger.warning(f"PROBLEMATIC RESPONSE: {content}")
 
         return str(final_revision["content"])
