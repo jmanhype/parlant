df98be9b3b601faa497dde49dc06e88ee308e3b3
Yam Marcovitz <yam@emcie.co>
2024-09-12 14:25:25 +0000

    Fix ContextualCorrelator scoping mixups

--- server/src/emcie/server/contextual_correlator.py
+++ server/src/emcie/server/contextual_correlator.py
@@ -4,20 +4,27 @@ from typing import Iterator
 
 from emcie.server.core.common import generate_id
 
+_UNINITIALIZED = 0xC0FFEE
+
 
 class ContextualCorrelator:
     def __init__(self) -> None:
         self._instance_id = generate_id()
-        self.scopes = contextvars.ContextVar[list[str]](
-            f"correlation_ids_{self._instance_id}",
-            default=[],
-        )
+        self.scopes = contextvars.ContextVar[list[str]](f"scope_ids_{self._instance_id}")
 
     @contextmanager
     def correlation_scope(self, scope_id: str) -> Iterator[None]:
-        self.scopes.get().append(scope_id)
+        scopes: int | list[str] = self.scopes.get(_UNINITIALIZED)
+
+        if scopes == _UNINITIALIZED:
+            self.scopes.set([])
+            scopes = self.scopes.get()
+
+        assert isinstance(scopes, list)
+
+        scopes.append(scope_id)
         yield
-        self.scopes.get().pop()
+        scopes.pop()
 
     @property
     def correlation_id(self) -> str:
