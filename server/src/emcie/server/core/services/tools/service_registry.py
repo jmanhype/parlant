3dc743525caac56227a82f3dfac6694b6431bb40
Dor Zohar <dor@emcie.co>
2024-10-15 14:39:19 +0000

    Add running_services object to return the instances that runs

--- server/src/emcie/server/core/services/tools/service_registry.py
+++ server/src/emcie/server/core/services/tools/service_registry.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from contextlib import AsyncExitStack
-from typing import Optional, Sequence, TypeAlias, TypedDict, cast
+from types import TracebackType
+from typing import Optional, Self, Sequence, TypeAlias, TypedDict, cast
 from typing_extensions import Literal
 
 from emcie.server.core.contextual_correlator import ContextualCorrelator
@@ -22,7 +23,7 @@ class ServiceRegistry(ABC):
     @abstractmethod
     async def update_tool_service(
         self,
-        name: str,
+        name: ServiceName,
         kind: ToolServiceKind,
         url: str,
         openapi_json: Optional[str] = None,
@@ -31,7 +32,7 @@ class ServiceRegistry(ABC):
     @abstractmethod
     async def read_tool_service(
         self,
-        name: str,
+        name: ServiceName,
     ) -> ToolService: ...
 
     @abstractmethod
@@ -42,20 +43,20 @@ class ServiceRegistry(ABC):
     @abstractmethod
     async def delete_service(
         self,
-        name: str,
+        name: ServiceName,
     ) -> None: ...
 
 
 class _ToolServiceDocument(TypedDict, total=False):
     id: ObjectId
     version: Version.String
-    name: str
+    name: ServiceName
     kind: ToolServiceKind
     url: str
     openapi_json: Optional[str]
 
 
-class ServiceRegistryDocument(ServiceRegistry):
+class ServiceDocumentRegistry(ServiceRegistry):
     VERSION = Version.from_string("0.1.0")
 
     def __init__(
@@ -63,7 +64,6 @@ class ServiceRegistryDocument(ServiceRegistry):
         database: DocumentDatabase,
         event_emitter_factory: EventEmitterFactory,
         correlator: ContextualCorrelator,
-        exit_stack: AsyncExitStack,
     ):
         self._tool_services_collection = database.get_or_create_collection(
             name="tool_services",
@@ -72,11 +72,47 @@ class ServiceRegistryDocument(ServiceRegistry):
 
         self._event_emitter_factory = event_emitter_factory
         self._correlator = correlator
-        self._exit_stack = exit_stack
+        self._exit_stack: AsyncExitStack
+        self._running_services: dict[ServiceName, ToolService] = {}
+
+    def _cast_to_specific_tool_service_class(
+        self,
+        service: ToolService,
+    ) -> OpenAPIClient | PluginClient:
+        if isinstance(service, OpenAPIClient):
+            return service
+        else:
+            return cast(PluginClient, service)
+
+    async def __aenter__(self) -> Self:
+        self._exit_stack = AsyncExitStack()
+        await self._exit_stack.__aenter__()
+
+        documents = await self._tool_services_collection.find({})
+
+        for document in documents:
+            service = self._deserialize_tool_service(document)
+            await self._exit_stack.enter_async_context(
+                self._cast_to_specific_tool_service_class(service)
+            )
+            self._running_services[document["name"]] = service
+
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> bool:
+        if self._exit_stack:
+            await self._exit_stack.__aexit__(exc_type, exc_value, traceback)
+            self._running_services.clear()
+        return False
 
     def _serialize_tool_service(
         self,
-        name: str,
+        name: ServiceName,
         service: ToolService,
     ) -> _ToolServiceDocument:
         return _ToolServiceDocument(
@@ -107,12 +143,11 @@ class ServiceRegistryDocument(ServiceRegistry):
 
     async def update_tool_service(
         self,
-        name: str,
+        name: ServiceName,
         kind: ToolServiceKind,
         url: str,
         openapi_json: Optional[str] = None,
     ) -> ToolService:
-        # TODO: In case a service is running and we override it, the service that is already running needs to exit.
         service: ToolService
 
         if kind == "openapi":
@@ -124,7 +159,11 @@ class ServiceRegistryDocument(ServiceRegistry):
                 event_emitter_factory=self._event_emitter_factory,
                 correlator=self._correlator,
             )
-            await self._exit_stack.enter_async_context(service)
+        await self._exit_stack.enter_async_context(
+            self._cast_to_specific_tool_service_class(service)
+        )
+
+        self._running_services[name] = service
 
         await self._tool_services_collection.update_one(
             filters={"name": {"$eq": name}},
@@ -136,24 +175,23 @@ class ServiceRegistryDocument(ServiceRegistry):
 
     async def read_tool_service(
         self,
-        name: str,
+        name: ServiceName,
     ) -> ToolService:
-        document = await self._tool_services_collection.find_one({"name": {"$eq": name}})
-        if not document:
+        if name not in self._running_services:
             raise ItemNotFoundError(item_id=UniqueId(name))
-
-        service = self._deserialize_tool_service(document)
-
-        return service
+        return self._running_services[name]
 
     async def list_tool_services(
         self,
     ) -> Sequence[tuple[ServiceName, ToolService]]:
-        documents = await self._tool_services_collection.find({})
+        return list(self._running_services.items())
 
-        return [(d["name"], self._deserialize_tool_service(d)) for d in documents]
+    async def delete_service(self, name: ServiceName) -> None:
+        if name in self._running_services:
+            service = self._running_services[name]
+            await (self._cast_to_specific_tool_service_class(service)).__aexit__(None, None, None)
+            del self._running_services[name]
 
-    async def delete_service(self, name: str) -> None:
         result = await self._tool_services_collection.delete_one({"name": {"$eq": name}})
         if not result.deleted_count:
             raise ItemNotFoundError(item_id=UniqueId(name))
