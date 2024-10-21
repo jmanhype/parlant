59f7fdaf72b32e26ada83fc2c937baf07d59aa0f
Menachem Brichta <menachem@emcie.co>
2024-10-21 13:49:24 +0000

    add more tests

--- server/src/emcie/server/api/chat/react-app/chat/src/components/sessions/sessions.test.tsx
+++ server/src/emcie/server/api/chat/react-app/chat/src/components/sessions/sessions.test.tsx
@@ -1,8 +1,9 @@
 import { cleanup, MatcherOptions, render } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it, Mock, vi } from "vitest";
 import Sessions from "./sessions.tsx";
 import '@testing-library/jest-dom/vitest';
 import { Matcher } from "vite";
+import useFetch from "@/hooks/useFetch.tsx";
 
 const sessionsArr = [
     { id: 'session1', title: 'Session One' },
@@ -10,24 +11,28 @@ const sessionsArr = [
 ];
 
 vi.mock('@/hooks/useFetch', () => ({
-    default: () => {
+    default: vi.fn(() => {
         return {
             data: {sessions: sessionsArr},
             refetch: vi.fn()
         }
-    }
+    })
 }));
 
 describe('Sessions Component', () => {
     let getAllByTestId: (id: Matcher, options?: MatcherOptions | undefined) => HTMLElement[];
     let getByTestId: (id: Matcher, options?: MatcherOptions | undefined) => HTMLElement;
+    let getByText: (id: Matcher, options?: MatcherOptions | undefined) => HTMLElement;
     let sessions: HTMLElement;
     let session: HTMLElement[];
+    let rerender: (ui: React.ReactNode) => void;
     
     beforeEach(() => {
         const utils = render(<Sessions agentId="" setSession={vi.fn()} sessionId="session1"/>);
         getAllByTestId = utils.getAllByTestId as (id: Matcher, options?: MatcherOptions | undefined) => HTMLElement[];
         getByTestId = utils.getByTestId as (id: Matcher, options?: MatcherOptions | undefined) => HTMLElement;
+        getByText = utils.getByText as (id: Matcher, options?: MatcherOptions | undefined) => HTMLElement;
+        rerender = utils.rerender;
         sessions = getByTestId('sessions');
         session = getAllByTestId('session');
     });
@@ -40,5 +45,27 @@ describe('Sessions Component', () => {
 
     it('component should have 2 sessions', () => {
         expect(session).toHaveLength(2);
+    });
+
+    it('component should show a loading indication on loading', () => {
+        (useFetch as Mock).mockImplementationOnce(() => ({
+            data: {sessions: sessionsArr},
+            loading: true,
+            refetch: vi.fn()
+        }));
+        rerender(<Sessions agentId="" setSession={vi.fn()} sessionId="session1"/>);
+        const loading = getByText('loading...');
+        expect(loading).toBeInTheDocument();
+    });
+
+    it('component should show error when it gets one', () => {
+        (useFetch as Mock).mockImplementationOnce(() => ({
+            data: {sessions: sessionsArr},
+            ErrorTemplate: () => <div>error</div>,
+            refetch: vi.fn()
+        }));
+        rerender(<Sessions agentId="" setSession={vi.fn()} sessionId="session1"/>);
+        const error = getByText('error');
+        expect(error).toBeInTheDocument();
     });
 });
