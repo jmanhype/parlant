32462ba85a0c1a1ad6c337b510630dfe7c640e89
Menachem Brichta <menachem@emcie.co>
2024-10-21 14:54:46 +0000

    test agent select component

--- server/src/emcie/server/api/chat/react-app/chat/src/components/agents-select/agents-select.test.tsx
+++ server/src/emcie/server/api/chat/react-app/chat/src/components/agents-select/agents-select.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
 import { describe, it, expect, vi } from 'vitest';
 import AgentsSelect from './agents-select.tsx';
 import '@testing-library/jest-dom/vitest';
@@ -15,17 +15,47 @@ vi.mock('@/hooks/useFetch', () => ({
 }))
 
 describe('Agent Select Component', () => {
-
+    afterEach(() => cleanup());
+    
     it('component should be rendered', () => {
         const {getByRole} = render(<AgentsSelect setSelectedAgent={vi.fn()}/>);
         const selectBox = getByRole('combobox');
         expect(selectBox).toBeInTheDocument();
     });
 
-    it('none should be selected', async () => {
-        const {getByRole} = render(<AgentsSelect setSelectedAgent={vi.fn()}/>);
-        const selectBox = getByRole('combobox');
-        expect(selectBox).toHaveTextContent('Select an agent');
+    it('selects the first agent if no value is provided', () => {
+        const mockSetSelectedAgent = vi.fn();
+        render(<AgentsSelect setSelectedAgent={mockSetSelectedAgent}/>);
+
+        expect(mockSetSelectedAgent).toHaveBeenCalledWith('agent1');
+    });
+
+    it('does not select the first agent if no value is provided', () => {
+        const mockSetSelectedAgent = vi.fn();
+        render(<AgentsSelect value='agent1' setSelectedAgent={mockSetSelectedAgent}/>);
+
+        expect(mockSetSelectedAgent).not.toHaveBeenCalled();
+    });
+    
+    it('2 options should be available as options', () =>{
+        const { getByText, getByRole } = render(<AgentsSelect setSelectedAgent={vi.fn()}/>);
+        const select = getByRole('combobox');
+        fireEvent.click(select);
+
+        expect(getByText('Agent One')).toBeInTheDocument();
+        expect(getByText('Agent Two')).toBeInTheDocument();
+    });
+    
+    it('calls setSelectedAgent when an agent is selected', () =>{
+        const mockSetSelectedAgent = vi.fn();
+        const { getByText, getByRole } = render(<AgentsSelect setSelectedAgent={mockSetSelectedAgent}/>);
+    
+        vi.clearAllMocks();
+
+        fireEvent.click(getByRole('combobox'));
+        fireEvent.click(getByText('Agent One'));
+
+        expect(mockSetSelectedAgent).toHaveBeenCalledWith('agent1');
     });
 
     it('agent1 should be selected', async () => {
