e7db4c90993c64dd34855880b3b92dca17671449
Dor Zohar <dor@emcie.co>
2024-11-06 13:48:00 +0000

    Implementing Huggingface to use cuda or mps in case available. otherwise cpu

--- src/parlant/adapters/nlp/hugging_face.py
+++ src/parlant/adapters/nlp/hugging_face.py
@@ -7,49 +7,52 @@ from parlant.core.nlp.tokenizer import Tokenizer
 from parlant.core.nlp.embedding import Embedder, EmbeddingResult
 
 
-class HuggingFaceEmbedder(Embedder):
+class HuggingFaceTokenizer(Tokenizer):
     def __init__(self, model_name: str) -> None:
         self.model_name = model_name
-        self._model = AutoModel.from_pretrained(model_name, attn_implementation="eager")
-        self._model.save_pretrained(os.environ.get("PARLANT_HOME", "/tmp"))
-        self._model.eval()
+
+        if torch.backends.mps.is_available():
+            self._device = torch.device("mps")
+        elif torch.cuda.is_available():
+            self._device = torch.device("cuda")
+        else:
+            self._device = torch.device("cpu")
+
+        save_dir = os.environ.get("PARLANT_HOME", "/tmp")
+        os.makedirs(save_dir, exist_ok=True)
 
         self._tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self._tokenizer.save_pretrained(os.environ.get("PARLANT_HOME", "/tmp"))
-
-    async def embed(
-        self,
-        texts: list[str],
-        hints: Mapping[str, Any] = {},
-    ) -> EmbeddingResult:
-        tokenized_texts = self._tokenizer.batch_encode_plus(
-            texts, padding=True, truncation=True, return_tensors="pt"
-        )
-
-        with torch.no_grad():
-            embeddings = self._model(**tokenized_texts).last_hidden_state[:, 0, :]
-
-        return EmbeddingResult(vectors=embeddings.tolist())
-
-
-class AutoTokenizerEstimatingTokenizer(Tokenizer):
-    def __init__(self, model_name: str) -> None:
-        self.model_name = model_name
-        self._tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self._tokenizer.save_pretrained(save_dir)
 
     async def tokenize(self, prompt: str) -> list[int]:
-        return list(map(int, cast(list[str], self._tokenizer.tokenize(prompt))))
+        return cast(list[int], self._tokenizer.encode(prompt, add_special_tokens=True))
 
     async def estimate_token_count(self, prompt: str) -> int:
         tokens = self._tokenizer.tokenize(prompt)
         return len(tokens)
 
 
-class JinaAIEmbedder(HuggingFaceEmbedder):
-    def __init__(self) -> None:
-        super().__init__(model_name="jinaai/jina-embeddings-v2-base-en")
+class HuggingFaceEmbedder(Embedder):
+    def __init__(self, model_name: str) -> None:
+        self.model_name = model_name
 
-        self._estimating_tokenizer = AutoTokenizerEstimatingTokenizer(self.model_name)
+        save_dir = os.environ.get("PARLANT_HOME", "/tmp")
+        os.makedirs(save_dir, exist_ok=True)
+
+        if torch.backends.mps.is_available():
+            self._device = torch.device("mps")
+        elif torch.cuda.is_available():
+            self._device = torch.device("cuda")
+        else:
+            self._device = torch.device("cpu")
+
+        self._model = AutoModel.from_pretrained(
+            pretrained_model_name_or_path=model_name, attn_implementation="eager"
+        ).to(self._device)
+        self._model.save_pretrained(save_dir)
+        self._model.eval()
+
+        self._tokenizer = HuggingFaceTokenizer(model_name=model_name)
 
     @property
     def id(self) -> str:
@@ -59,5 +62,25 @@ class JinaAIEmbedder(HuggingFaceEmbedder):
     def max_tokens(self) -> int:
         return 8192
 
-    def get_tokenizer(self) -> AutoTokenizerEstimatingTokenizer:
-        return self._estimating_tokenizer
+    def get_tokenizer(self) -> HuggingFaceTokenizer:
+        return self._tokenizer
+
+    async def embed(
+        self,
+        texts: list[str],
+        hints: Mapping[str, Any] = {},
+    ) -> EmbeddingResult:
+        tokenized_texts = self._tokenizer._tokenizer.batch_encode_plus(
+            texts, padding=True, truncation=True, return_tensors="pt"
+        )
+        tokenized_texts = {key: value.to(self._device) for key, value in tokenized_texts.items()}
+
+        with torch.no_grad():
+            embeddings = self._model(**tokenized_texts).last_hidden_state[:, 0, :]
+
+        return EmbeddingResult(vectors=embeddings.tolist())
+
+
+class JinaAIEmbedder(HuggingFaceEmbedder):
+    def __init__(self) -> None:
+        super().__init__("jinaai/jina-embeddings-v2-base-en")
