38a695cf752ce997f941e0aa5b84f76b85074745
Dor Zohar <dor@emcie.co>
2024-12-22 20:31:23 +0000

    Add consult expert with utter

--- examples/modules/tech_store.py
+++ examples/modules/tech_store.py
@@ -1,3 +1,4 @@
+import httpx
 from lagom import Container
 
 from parlant.core.background_tasks import BackgroundTaskService
@@ -14,12 +15,35 @@ def list_categories(context: ToolContext) -> ToolResult:
     return ToolResult(["laptops", "chairs"])
 
 
+@tool
+async def consult_expert(context: ToolContext, user_query: str) -> ToolResult:
+    """
+    This is an example for using the utterances feature
+    """
+
+    async with httpx.AsyncClient(follow_redirects=True, timeout=30) as client:
+        (
+            await client.post(
+                f"https://localhost:8800/sessions/{context.session_id}/events",
+                json={
+                    "kind": "message",
+                    "source": "ai_agent",
+                    "actions": [
+                        {"action": "Tell the user that I'm thinking", "reason": "buy_time"}
+                    ],
+                },
+            )
+        ).raise_for_status().json()
+
+    return ToolResult(data="Best laptop is mac")
+
+
 async def initialize_module(container: Container) -> None:
     global server_instance
     _background_task_service = container[BackgroundTaskService]
 
     server = PluginServer(
-        tools=[list_categories],
+        tools=[list_categories, consult_expert],
         port=8095,
         host="127.0.0.1",
     )
