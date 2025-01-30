b81499bbe5aa345b3cffb5ed7f599404978e1081
Yam Marcovitz <yam@emcie.co>
2025-01-30 12:09:11 +0000

    Temporarily use a transient fragment store
    Signed-off-by: Yam Marcovitz <yam@emcie.co>

--- src/parlant/bin/server.py
+++ src/parlant/bin/server.py
@@ -30,6 +30,7 @@ from pathlib import Path
 import sys
 import uvicorn
 
+from parlant.adapters.db.transient import TransientDocumentDatabase
 from parlant.adapters.vector_db.chroma import ChromaDatabase
 from parlant.core.engines.alpha import hooks
 from parlant.core.engines.alpha import guideline_proposer
@@ -292,12 +293,6 @@ async def setup_container(nlp_service_name: str, log_level: str) -> AsyncIterato
             PARLANT_HOME_DIR / "sessions.json",
         )
     )
-    fragment_db = await EXIT_STACK.enter_async_context(
-        JSONFileDocumentDatabase(
-            LOGGER,
-            PARLANT_HOME_DIR / "fragments.json",
-        )
-    )
     guidelines_db = await EXIT_STACK.enter_async_context(
         JSONFileDocumentDatabase(LOGGER, PARLANT_HOME_DIR / "guidelines.json")
     )
@@ -320,7 +315,9 @@ async def setup_container(nlp_service_name: str, log_level: str) -> AsyncIterato
     )
     c[TagStore] = await EXIT_STACK.enter_async_context(TagDocumentStore(tags_db))
     c[CustomerStore] = await EXIT_STACK.enter_async_context(CustomerDocumentStore(customers_db))
-    c[FragmentStore] = await EXIT_STACK.enter_async_context(FragmentDocumentStore(fragment_db))
+    c[FragmentStore] = await EXIT_STACK.enter_async_context(
+        FragmentDocumentStore(TransientDocumentDatabase())
+    )
     c[GuidelineStore] = await EXIT_STACK.enter_async_context(GuidelineDocumentStore(guidelines_db))
     c[GuidelineToolAssociationStore] = await EXIT_STACK.enter_async_context(
         GuidelineToolAssociationDocumentStore(guideline_tool_associations_db)
