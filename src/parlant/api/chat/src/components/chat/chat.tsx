c56d4c595df8bcefb7c92ae55d5aef2da50a4312
Menachem Brichta <menachem@emcie.co>
2025-02-02 10:25:31 +0000

    change dates border
    Signed-off-by: Dor Zohar <dor@emcie.co>

--- src/parlant/api/chat/src/components/chat/chat.tsx
+++ src/parlant/api/chat/src/components/chat/chat.tsx
@@ -39,9 +39,9 @@ const emptyPendingMessage: () => EventInterface = () => ({
 const DateHeader = ({date, isFirst, bgColor}: {date: string | Date; isFirst: boolean; bgColor?: string}): ReactElement => {
 	return (
 		<div className={twMerge('text-center flex min-h-[30px] z-[1] bg-main h-[30px] pb-[4px] ps-[10px] mb-[60px] pt-[4px] mt-[76px] sticky -top-[1px]', isFirst && 'pt-[1px] !mt-0', bgColor)}>
-			<hr className='h-full -translate-y-[-50%] flex-1' />
+			<div className='[box-shadow:0_-0.6px_0px_0px_#EBECF0] h-full -translate-y-[-50%] flex-1 ' />
 			<div className='w-[136px] border-[0.6px] border-muted font-light text-[12px] bg-white text-[#656565] flex items-center justify-center rounded-[6px]'>{getDateStr(date)}</div>
-			<hr className='h-full -translate-y-[-50%] flex-1' />
+			<div className='[box-shadow:0_-0.6px_0px_0px_#EBECF0] h-full -translate-y-[-50%] flex-1' />
 		</div>
 	);
 };
